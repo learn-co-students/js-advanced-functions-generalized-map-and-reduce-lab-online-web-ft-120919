23f2488541521a4bb8f091c14710c8f745e075fa
chefkasperson <chefkasperson@gmail.com>
2020-04-04 20:00:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(arr, fn) {
+  let res = []
+
+  for (let i = 0; i < arr.length; i++) {
+    res.push(fn(arr[i]))
+  }
+  return res
+}
+
+function reduce(arr, fn, starting) {
+  let res = (!!starting) ? starting : arr[0]
+  let i = (!!starting) ? 0 : 1 
+
+  for (; i < arr.length; i++) {
+    res = fn(arr[i], res)
+  }
+  return res
+}
