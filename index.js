71145bcf631615054b4eaae4c4f7de7e457461be
ben20262 <ben20262@aim.com>
2020-04-06 16:56:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Add your functions here
+function map(array, func) {
+    return array.map(func)
+}
+
+function reduce(array, func, start=0) {
+    let answer = array.reduce(func)
+    return answer === !!answer ? answer : answer + start
+}
