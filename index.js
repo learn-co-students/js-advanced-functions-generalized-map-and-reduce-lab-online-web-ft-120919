ebb3c60d670547f178664b648032201258f09a64
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-06 17:54:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(srcArray, func) {
+  let mapArray = []
+  srcArray.forEach(index => {
+    mapArray.push(func(index))
+  })
+  return mapArray
+}
+
+function reduce(srcArray, func, startingPoint) {
+  let total
+  let i
+
+  if (!!startingPoint) {
+    total = startingPoint
+    i = 0
+
+  } else {
+    total = func(srcArray[0], srcArray[1])
+    i = 2
+  }
+  for (i; i < srcArray.length; i++) {
+    total = func(total, srcArray[i])
+  }
+  return total
+}
