0c790a24510e72c84e2ecb0d91ed3e81ee87f8b1
rick <github email address>
2020-04-06 21:55:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+function map(input, funct) {
+	return input.map(funct);
+}
+
+function reduce(input, func, sp=0) {
+    let sv = sp
+	return input.reduce(function(pasedIn, item) {
+        console.log(pasedIn, item)
+     return func(item, pasedIn)  
+    }, sv)
+}
