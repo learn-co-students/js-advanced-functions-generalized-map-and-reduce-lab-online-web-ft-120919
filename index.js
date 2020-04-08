3a8e39103783afbddad015037e5c481b11d18be8
Adrian082818 <mohabiradrian@gmail.com>
2020-04-08 11:38:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+
+function map(sourceArray, func) {
+   return sourceArray.map(element => func(element))
+};
+
+function reduce(sourceArray, func, startingValue=0) {
+   if (startingValue) {
+       return sourceArray.reduce((acc, next) => {
+        return func(acc, next)
+       }, startingValue)
+   } else {
+       return sourceArray.reduce((acc, next) => {
+           return func(acc, next)
+       })
+   }
+}   
