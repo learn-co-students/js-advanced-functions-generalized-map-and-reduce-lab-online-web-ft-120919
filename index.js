f28df9e955e3d899e5ca0a798a1225e2bd309a79
93geraldtee <93geraldtee@gmail.com>
2020-04-11 17:49:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(array, fn){
+    let newArray = []
+
+    for (let i = 0; i < array.length; i++){
+        let element = array[i]
+        newArray.push(fn(element))
+    }
+    return newArray;
+}
+
+function reduce(array, fn, startingValue){
+    let r = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+
+    for (; i < array.length; i++){
+        r = fn(array[i], r)
+    }
+    return r;
+}
