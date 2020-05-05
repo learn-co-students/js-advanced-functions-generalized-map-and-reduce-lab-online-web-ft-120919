722c9d15a92f6933791ce6b5cbec4e0787c72308
Zach Cullen <zacullen@gmail.com>
2020-05-05 15:27:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+ function map(obj, func) {
+   let result = []
+   for (let i = 0; i < obj.length; i++) {
+     let element = obj[i]
+     result.push(func(element))
+   }
+   return result
+ }
+
+ function reduce(obj, func, start) {
+   let accum = !!start ? start : obj[0]
+   let i = !!start ? 0 : 1
+   for (; i < obj.length; i++) {
+     accum = func(obj[i], accum)
+   }
+   return accum
+ }
